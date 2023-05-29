1edb9835fe8913becc91d7a187d5846e9a1e793e
Tiberiu Popescu <tiberiu1204@gmail.com>
2023-05-29 19:15:27 +0300

    Finished hero prototype

---
+++ src/Button.tsx
@@ -0,0 +1,60 @@
+import "./Button.css";
+
+interface Props {
+  text: string;
+  link: string;
+  style: "primary" | "secondary";
+}
+
+const Button = ({ text, link, style }: Props) => {
+  return (
+    <a className={"button button__" + style + "Theme"} href={link}>
+      <span className="button-text">{text}</span>
+      <svg
+        width="5"
+        height="4"
+        viewBox="0 0 5 4"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="arrow-svg"
+      >
+        <g className={"arrow-" + style} clip-path="url(#clip0_1_2)">
+          <rect
+            className="arrow-body"
+            y="1.66957"
+            width="3.47826"
+            height="0.695652"
+            rx="0.347826"
+          />
+          <g className="arrow-tip">
+            <rect
+              className="arrow-tip-up"
+              x="1.5666"
+              y="0.491913"
+              width="0.695652"
+              height="2.78261"
+              rx="0.347826"
+              transform="rotate(-45 1.5666 0.491913)"
+            />
+            <rect
+              className="arrow-tip-down"
+              x="3.55388"
+              y="1.54082"
+              width="0.695652"
+              height="2.78261"
+              rx="0.347826"
+              transform="rotate(45 3.55388 1.54082)"
+            />
+          </g>
+        </g>
+        <defs>
+          <clipPath id="clip0_1_2">
+            <rect width="4.04591" height="4" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    </a>
+  );
+};
+
+export default Button;
--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,4 +1,5 @@
 import "./Hero.css";
+import Button from "./Button";
 
 const Hero = () => {
   return (
@@ -9,18 +10,24 @@ const Hero = () => {
             Educație <br /> de calitate <br /> copilului tău
           </h1>
           <h2 className="hero-description">
-            Alege să investești în educația copilului tău, prin a îl incrie – la
+            Alege să investești în educația copilului tău, prin a îl încrie – la
             fel ca mulți alți părinți mulțumiți – la noi la afterschool, unde se
             va dezvolta armonios pe plan intelectual, social, și emotional.
           </h2>
-          <a className="button button__primaryTheme" href="tel:+40766553533">
-            Sună acum
-          </a>
-          <a className="button button__secondaryTheme" href="#">
-            Program și oferte
-          </a>
+          <Button text="Sună acum" link="tel:+40766553533" style="primary" />
+          <Button
+            text="Program și oferte"
+            link="tel:+40766553533"
+            style="secondary"
+          />
         </div>
-        <div className="hero-image"></div>
+        <div className="hero-visual">
+          <img
+            src="./src/assets/hero-img.jpg"
+            alt="hero"
+            className="hero-image"
+          />
+        </div>
       </div>
     </section>
   );
